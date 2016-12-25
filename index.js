7099c6e082869a8500d618e3a43eda1025a239ef
Flatiron School <github email address>
2016-12-25 12:17:04 -0500

    done

--- index.js
+++ index.js
@@ -11,3 +11,37 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+function addToPlaylist(playlist, episode) {
+  // episodes.map(episode => playlist.concat(episode));
+  return playlist.concat(episode)
+}
+
+
+
+function removeFromPlaylist(playlist, episode){
+  return playlist.filter(function(episode_element){
+    if (episode_element !== episode){
+      // debugger;
+      return episode_element
+    }
+  })
+  // debugger;
+}
+
+
+function getNextEpisodeInPlaylist(playlist) {
+  return playlist[0]
+}
+
+
+function bingeWatch(playlist){
+
+  if (!getNextEpisodeInPlaylist(playlist)) {
+    return 'Please let there be more!'
+  } else {
+    var playlist = removeFromPlaylist(playlist, getNextEpisodeInPlaylist(playlist))
+    return bingeWatch(playlist)
+  }
+
+}
